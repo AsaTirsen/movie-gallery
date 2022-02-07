93f374b51c2a5e4a22842b5d3d413f6f0cfb8f78
Asa Tirsen <asa.tirsen@gmail.com>
2022-02-07 17:38:30 +0100

    responsive

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,4 +1,5 @@
 import MainNavigation from "./MainNavigation";
+import Menu from "./BurgerMenu";
 import classes from "./Layout.module.css";
 import Footer from "./Footer";
 
--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,12 +1,22 @@
 import { useContext } from "react";
 import Link from "next/link";
 import classes from "./MainNavigation.module.css";
+import React, { useState } from "react";
+import { MdClose } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
 
 function MainNavigation() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen((prev) => !prev);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
   return (
     <header className={classes.header}>
       <h2 className={classes.title}>The Movie Finder</h2>
-      <nav>
+      <nav className={classes.navBarLarge}>
         <ul>
           <li>
             <Link href="/">Movies</Link>
@@ -19,6 +29,51 @@ function MainNavigation() {
           </li>
           <li>
             <Link href="/">Sign in</Link>
+          </li>
+        </ul>
+      </nav>
+      <nav className={classes.navBarSmall}>
+        <button className={classes.button} onClick={handleToggle}>
+          {navbarOpen ? (
+            <MdClose style={{ color: "#fff", width: "40px", height: "40px" }} />
+          ) : (
+            <FiMenu
+              style={{ color: "#7b7b7b", width: "40px", height: "40px" }}
+            />
+          )}
+        </button>
+        <ul
+          className={`${classes.menuNav} ${navbarOpen ? classes.showMenu : ""}`}
+        >
+          <li className={classes.listitem}>
+            <Link
+              href="/"
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              Movies
+            </Link>
+          </li>
+          <li className={classes.listitem}>
+            <Link
+              href="/"
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              TV-series
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/"
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              Actors
+            </Link>
           </li>
         </ul>
       </nav>
