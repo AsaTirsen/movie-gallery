9193c793f25ad426053a7ca56a7053356affbb59
Asa Tirsen <asa.tirsen@gmail.com>
2022-02-07 09:31:30 +0100

    small css change

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import styles from "../styles/Home.module.css";
 import MovieList from "../components/movies/MovieList";
 import { useState, useEffect } from "react";
 import Search from "../components/movies/Search";
+import { push as Menu } from 'react-burger-menu'
+import React from "react";
+
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
